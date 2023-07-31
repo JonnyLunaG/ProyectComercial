f0d24cb79245c885cdd441af910c08184351180a
JonnyLunaG <jonnylunag@gmail.com>
2023-07-31 04:20:53 -0500

    "se hace la macroestreuctura de la apagin"

---
+++ views/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    https://www.ferreteriasamir.com/
+</footer>
---
+++ views/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <--?php include 'views/header.php'; ?>
+
+    <h1>Ferreteria Comercial H'Lunas</h1>
+
+    <ul>
+    <li><a href="ventas.php">Ventas</a></li>
+    <li><a href="clientes.php">Clientes</a></li>
+    <li><a href="proveedores.php">Proveedores</a></li>
+    </ul>
+
+    <--?php include 'views/footer.php'; ?>
+
+    
+
+</body>
+</html>
